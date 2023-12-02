8d513469af70eb8551f0c259b3032b1feaf47795
Bjarki Baldursson Harksen <bjarki@harksen.is>
2023-12-02 00:09:51 +0000

    [2023] Day 1: Trebuchet?!

---
+++ 2023/01/1.exs
@@ -0,0 +1,14 @@
+get_number = fn line ->
+  case String.split(line, ~r/[^0-9]*/, trim: true) do
+    [] -> 0
+    digits -> 
+      {first, _} = List.first(digits) |> Integer.parse
+      {last, _} = List.last(digits) |> Integer.parse
+      10*first + last
+  end
+end
+
+IO.stream()
+  |> Enum.map(get_number)
+  |> Enum.sum
+  |> IO.puts
---
+++ 2023/01/2.exs
@@ -0,0 +1,30 @@
+parse_digit = fn digit ->
+  case digit do
+    "one" -> 1
+    "two" -> 2
+    "three" -> 3
+    "four" -> 4
+    "five" -> 5
+    "six" -> 6
+    "seven" -> 7
+    "eight" -> 8
+    "nine" -> 9
+    _ -> Integer.parse(digit) |> elem(0)
+  end
+end
+
+get_number = fn line ->
+  numbers = ~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
+    |> Regex.scan(line, capture: :all_but_first)
+    |> Enum.map(&hd/1)
+
+  first = List.first(numbers) |> parse_digit.()
+  last = List.last(numbers) |> parse_digit.()
+
+  10*first + last
+end
+
+IO.stream()
+  |> Enum.map(get_number)
+  |> Enum.sum
+  |> IO.puts
